2b2389080c73593b6747392942526ae684a254dd
varunFalcon <falcondsharma@gmail.com>
2012-08-12 15:04:52 +0530

    count function added

--- CreateLinkedList.java
+++ CreateLinkedList.java
@@ -1,35 +1,39 @@
 package practice.leanLinkedList;
 
-
 public class CreateLinkedList {
 	Node head = null;
 
 	/**
 	 * @param args
 	 */
-	
-	public void addNode(int input){
+
+	public void addNode(int input) {
 		Node end = new Node(input);
-		if(this.head==null)
-		{
-		this.head = end;
-		return;
+		if (this.head == null) {
+			this.head = end;
+			return;
 		}
-		    Node h = this.head;
-		while(h.next!= null){
+		Node h = this.head;
+		while (h.next != null) {
 			h = h.next;
 		}
-		h.next=end;
-}
-	 public void displayList(){
-		 Node printNode= this.head;
+		h.next = end;
+	}
+
+	public void displayList() {
+		Node printNode = this.head;
 		System.out.println("Displaying the linked list nodes values :");
-		while(printNode!= null){
+		while (printNode != null) {
 			System.out.println(printNode.value);
-			printNode=printNode.next;	
+			printNode = printNode.next;
+		}
 	}
+
+	public int countNodes() {
+		int count = 0;
+		while (this.head.next != null) {
+			count++;
+		}
+		return count;
 	}
 }
-
-
-
